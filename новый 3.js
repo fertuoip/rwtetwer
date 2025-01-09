c8d1be93fb14da705c91db3402eb4ff128c507f6
fertuoip <145692113+fertuoip@users.noreply.github.com>
2025-01-09 23:59:53 +0500

    Add files via upload

--- новый 3.js
+++ новый 3.js
@@ -6,7 +6,7 @@ describe("About Functions (about_functions.js)", function() {
       return a + b;
     }
 
-    expect(add(1, 2)).toBe(FILL_ME_IN);
+    expect(add(1, 2)).toBe(3);
   });
 
   it("should know internal variables override outer variables", function () {
@@ -21,9 +21,9 @@ describe("About Functions (about_functions.js)", function() {
       return message;
     }
 
-    expect(getMessage()).toBe(FILL_ME_IN);
-    expect(overrideMessage()).toBe(FILL_ME_IN);
-    expect(message).toBe(FILL_ME_IN);
+    expect(getMessage()).toBe("Outer");
+    expect(overrideMessage()).toBe("Inner");
+    expect(message).toBe("Outer");
   });
 
   it("should have lexical scoping", function () {
@@ -35,7 +35,7 @@ describe("About Functions (about_functions.js)", function() {
       }
       return childfunction();
     }
-    expect(parentfunction()).toBe(FILL_ME_IN);
+    expect(parentfunction()).toBe("local");
   });
 
   it("should use lexical scoping to synthesise functions", function () {
@@ -50,7 +50,7 @@ describe("About Functions (about_functions.js)", function() {
     let mysteryFunction3 = makeMysteryFunction(3);
     let mysteryFunction5 = makeMysteryFunction(5);
 
-    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(FILL_ME_IN);
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(23);
   });
 
   it("should allow extra function arguments", function () {
@@ -59,13 +59,13 @@ describe("About Functions (about_functions.js)", function() {
       return firstArg;
     }
 
-    expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+    expect(returnFirstArg("first", "second", "third")).toBe("first");
 
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
     }
 
-    expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+    expect(returnSecondArg("only give first arg")).toBe(undefined);
 
     function returnAllArgs() {
       let argsArray = [];
@@ -89,10 +89,122 @@ describe("About Functions (about_functions.js)", function() {
     };
 
     let praiseSinger = { givePraise: appendRules };
-    expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
+    expect(praiseSinger.givePraise("John")).toBe("John rules!");
 
     praiseSinger.givePraise = appendDoubleRules;
-    expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
+    expect(praiseSinger.givePraise("Mary")).toBe("Mary totally rules!");
 
+  });
+});
+
+describe("About Functions And Closure (about_functions_and_closure.js)", function() {
+  it("defining functions directly", function() {
+    let result = "a";
+    function changeResult() {
+      // the ability to access variables defined in the same scope as the function is known as 'closure'
+      result = "b";
+    };
+    changeResult();
+    // what is the value of result?
+    expect("b").toBe(result);
+  });
+
+  it("assigning functions to variables", function() {
+    let triple = function(input) {
+      return input * 3;
+    };
+    // what is triple 4?
+    expect(12).toBe(triple(4));
+  });
+
+  it("self invoking functions", function() {
+    let publicValue = "shared";
+
+    // self invoking functions are used to provide scoping and to alias variables
+    (function(pv) {
+      let secretValue = "password";
+      // what is the value of pv?
+      expect("shared").toBe(pv);
+      // is secretValue available in this context?
+      expect("string").toBe(typeof(secretValue));
+      // is publicValue available in this context?
+      expect("string").toBe(typeof(publicValue));
+    })(publicValue);
+
+    // is secretValue available in this context?
+    expect("undefined").toBe(typeof(secretValue));
+    // is publicValue available in this context?
+    expect("string").toBe(typeof(publicValue));
+  });
+
+  it("arguments array", function() {
+    let add = function() {
+      let total = 0;
+      for(let i = 0; i < arguments.length; i++) {
+        // complete the implementation of this method so that it returns the sum of its arguments
+        total += arguments[i];
+      }
+        return total;
+    };
+
+    // add 1,2,3,4,5
+    expect(15).toBe(add(1,2,3,4,5));
+    // add 4,7,-2
+    expect(9).toBe(add(4,7,-2));
+  });
+
+  it("using call to invoke function",function(){
+    let invokee = function( message ){
+      return this + message;  
+    };
+    
+    //another way to invoke a function is to use the call function which allows 
+    //you to set the caller's "this" context.  Call can take any number of arguments: 
+    //the first one is always the context that this should be set to in the called
+    //function, and the arguments to be sent to the function, multiple arguments are separated by commas.
+    let result = invokee.call("I am this!", "Where did it come from?");
+
+    // what will the value of invokee's this be?
+    expect("I am this!Where did it come from?").toBe(result);
+  });
+
+  it("using apply to invoke function",function(){
+    let invokee = function( message1, message2 ){
+      return this + message1 + message2;  
+    };
+
+    //similar to the call function is the apply function.  Apply only has two
+    //arguments:  the first is the context that this should be set to in the called
+    //function and the second is the array of arguments to be passed into the called function.
+    let result = invokee.apply("I am this!", ["I am arg1","I am arg2"]);
+
+    // what will the value of invokee's this be?
+    expect("I am this!I am arg1I am arg2").toBe(result);
+  });
+});
+describe("About Scope (about_scope.js)", function() {
+  thisIsAGlobalVariable = 77;
+
+  it("global variables", function() {
+    // is thisIsAGlobalVariable defined in this scope?
+    expect(77).toBe(thisIsAGlobalVariable);
+  });
+
+  it("variables declared inside of a function", function() {
+    let outerVariable = "outer";
+
+    // this is a self-invoking function. Notice that it calls itself at the end ().
+    (function() {
+      let innerVariable = "inner";
+      // is outerVariable defined in this scope?
+      expect("outer").toBe(outerVariable);
+      // is innerVariable defined in this scope?
+      expect("inner").toBe(innerVariable);
+    })();
+
+    // is outerVariable defined in this scope?
+    expect("outer").toBe(outerVariable);
+    // is innerVariable defined in this scope?
+    expect("undefined").toBe(typeof(innerVariable));
   });
 });
